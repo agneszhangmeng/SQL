28f4b2c72dfbbef6fb11bb1305dcd49fea65cae5
agneszhangmeng <mz435@cornell.edu>
2017-02-06 19:38:15 +0800

    Update kitchen.sql

--- kitchen.sql
+++ kitchen.sql
@@ -16,3 +16,22 @@ with order_info as (
 ) select * from order_info oo
     where oo.day >= to_date(20170101, 'yyyymmdd') and oo.unitname1 = 'Kitchen' and  oo.actualsales >= 20 
     
+ 
+with order_info as (
+        select s.externalID, to_char(o.orderedAt,'yyyyMMdd') as orderDay, 
+            decode(oi.quantity,0,'NO','YES') status
+          from skus s
+          left join ORDER_ITEMS oi
+              on s.id = oi.skuid
+          left join ORDERS o
+              on oi.orderid = o.id
+        group by 1,2
+        having orderDay >= '20170101' and s.externalID = '41762'
+    )   select b.externalID, b.orderDay, 
+        case when 
+        from deal_sku a
+        left join order_info b
+        on a.dealskuseq = b.externalID
+        where a.unitedsku = 'Y' 
+        group by 1
+    
