ae13bc4827bbb1b9db6df302facd7700fd86ef6c
agneszhangmeng <mz435@cornell.edu>
2017-02-07 17:03:21 +0800

    Update kitchen.sql

--- kitchen.sql
+++ kitchen.sql
@@ -1,3 +1,4 @@
+/* fast moving skus in kitchen */ 
 with order_info as (
     select 
         to_char(o.orderedat, 'yyyymmdd') day, 
@@ -15,28 +16,7 @@ with order_info as (
     group by 1,2,3
 ) select * from order_info oo
     where oo.day >= to_date(20170101, 'yyyymmdd') and oo.unitname1 = 'Kitchen' and  oo.actualsales >= 20 
-    
- 
-with order_info as (
-        select s.externalID, to_char(o.orderedAt,'yyyyMMdd') as orderDay, 
-            decode(oi.quantity,0,'NO','YES') status
-          from skus s
-          left join ORDER_ITEMS oi
-              on s.id = oi.skuid
-          left join ORDERS o
-              on oi.orderid = o.id
-        group by 1,2
-        having orderDay >= '20170101' and s.externalID = '41762'
-    )   select b.externalID, b.orderDay, 
-        case when 
-        from deal_sku a
-        left join order_info b
-        on a.dealskuseq = b.externalID
-        where a.unitedsku = 'Y' 
-        group by 1
-        
-        
-        
+      
 with temp as (
     select a.dealskuseq, to_char(o.orderedAt,'yyyyMMdd') as orderDay, b.unitname1, 
      count(oi.quantity) as sales, count(*) as total
@@ -84,4 +64,20 @@ select to_char(o.orderedAt,'yyyyMMdd') as orderDay, s.externalID,
           left join ORDERS o
               on oi.orderid = o.id
         where orderDay >= '20170101' and s.externalID = '46464'
-    
+/* select percentage of 0 */ 
+select d.DT as Date, s.externalID, b.unitname1,
+            count(oi.quantity) actualsales
+        from dim_date d 
+        left join ORDERS o
+            on cast(to_char(o.orderedAt,'yyyyMMdd') as int) = d.DT 
+        left join ORDER_ITEMS oi
+            on oi.orderid = o.id
+        left join SKUS s
+            on s.id = oi.skuid
+        left join deal_sku a 
+            on a.dealskuseq = s.externalid
+        left join MANAGEMENT_CATEGORY_HIER_CURR b 
+            on a.managecategoryseq = b.mngcateid
+        group by 1,2,3
+        having Date >= '20170205' and b.unitname1 = 'Kitchen'
+        
