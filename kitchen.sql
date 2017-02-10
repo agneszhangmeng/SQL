55e2806210cd9929e5ba743b639e85add57c03ca
agneszhangmeng <mz435@cornell.edu>
2017-02-10 10:42:48 +0800

    Update kitchen.sql

--- kitchen.sql
+++ kitchen.sql
@@ -167,3 +167,61 @@ with percentage as (
 ) select p.externalId, p.week, count(p.sale_times) as weektimes 
     from percentage p
     group by 1,2
+
+/* week period for three months */ 
+with percentage as (
+    with sales as (
+            select 
+                a.externalId, 
+                to_char(b.orderedAt,'yyyyMMdd') as orderDay,
+                case 
+                    when orderDay >= 20161001 and orderDay <= 20161007 then 1
+                    when orderDay >= 20161008 and orderDay <= 20161014 then 2
+                    when orderDay >= 20161015 and orderDay <= 20161021 then 3
+                    when orderDay >= 20161022 and orderDay <= 20161028 then 4
+                    when orderDay >= 20161029 and orderDay <= 20161104 then 5
+                    when orderDay >= 20161105 and orderDay <= 20161111 then 6
+                    when orderDay >= 20161112 and orderDay <= 20161118 then 7
+                    when orderDay >= 20161119 and orderDay <= 20161125 then 8
+                    when orderDay >= 20161126 and orderDay <= 20161202 then 9
+                    when orderDay >= 20161203 and orderDay <= 20161209 then 10
+                    when orderDay >= 20161210 and orderDay <= 20161216 then 11
+                    when orderDay >= 20161217 and orderDay <= 20161223 then 12
+                    when orderDay >= 20161224 and orderDay <= 20161230 then 13
+                    when orderDay >= 20161231 and orderDay <= 20170106 then 14
+                    when orderDay >= 20170107 and orderDay <= 20170113 then 15
+                    when orderDay >= 20170114 and orderDay <= 20170120 then 16
+                    when orderDay >= 20170121 and orderDay <= 20170127 then 17
+                    end as week,
+                count(b.quantity) as sale_times
+                from skus a 
+                    left join (
+                        select
+                            o1.orderedAt,
+                            o2.skuId, 
+                            o2.quantity
+                        from orders o1 
+                            join order_items o2 on o1.id = o2.orderid
+                        where o1.orderedAt>='20170101' and o1.status<>'CLOSED'
+                    ) b on a.id = b.skuId
+                    join deal_sku c on a.externalId = c.dealskuseq 
+                    join MANAGEMENT_CATEGORY_HIER_CURR d on c.managecategoryseq = d.mngcateid
+                    where d.unitname1 = 'Kitchen'  
+                group by a.externalId,orderDay
+        ) 
+    select 
+        a.dt,
+        s.week,
+        a.externalId,
+        s.sale_times
+    from 
+        ( select distinct d.dt, e.externalId
+            from sales e 
+            join dim_date d on 1=1
+            where d.dt >=20161001 and d.dt<=20170127
+        ) a 
+        left join sales s on a.dt = s.orderDay and a.externalId = s.externalId
+    order by a.externalId, a.dt 
+) select p.externalId, p.week, count(p.sale_times) as weektimes 
+    from percentage p
+    group by 1,2
