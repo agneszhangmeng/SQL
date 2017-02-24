8d8e923281b4380e7c8320a8a0361e9107a6266a
agneszhangmeng <mz435@cornell.edu>
2017-02-24 12:58:50 +0800

    Update slow_kitchen.sql

--- slow_kitchen.sql
+++ slow_kitchen.sql
@@ -53,3 +53,49 @@
         join DLF_DEMAND_FORECASTING_ITEM_PV_ROLLING p on p.skuseq = a.externalId 
         where d.unitname1 = 'Kitchen'  and c.skutype = 'NORMAL' and c.salestatus = 'ACTIVE' 
     group by a.externalId
+    
+    
+   # month percentage 
+   with temp as ( 
+    with sales as (
+            select 
+                a.externalId, 
+                to_char(b.orderedAt, 'MM') as month,
+                d.unitname2, 
+                sum(b.quantity) as sale_times
+                from skus a 
+                    left join (
+                        select
+                            o1.orderedAt,
+                            o2.skuId, 
+                            o2.quantity
+                        from orders o1 
+                            join order_items o2 on o1.id = o2.orderid
+                        where  o1.orderedAt>='20160701' and o1.orderedAt <= '20170131' and o1.status<>'CLOSED'
+                    ) b on a.id = b.skuId
+                    left join deal_sku c on a.externalId = c.dealskuseq 
+                    left join MANAGEMENT_CATEGORY_HIER_CURR d on c.managecategoryseq = d.mngcateid
+                    where d.unitname1 = 'Kitchen' 
+                group by 1,2,3
+            ) 
+        select 
+            a.MM,
+            a.externalId,
+            s.unitname2,
+            s.sale_times
+        from 
+            ( select distinct d.MM, e.externalId
+                from sales e 
+                join dim_date d on 1=1
+                where (d.MM >= 07 and d.MM <= 12 ) or d.MM = 01 
+            ) a 
+            left join sales s on a.MM = s.month and a.externalId = s.externalId
+    ) select 
+        t.dt, t.externalId, 
+        r.unitname2, t.sale_times
+        from 
+            (select distinct m.unitname2, t.externalId
+                from temp t
+                join MANAGEMENT_CATEGORY_HIER_CURR m on 1=1
+            ) r
+            left join temp t on t.externalId = r.externalId
