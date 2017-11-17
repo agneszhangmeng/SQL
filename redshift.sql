47337677c792606a427612a667a43d242ac2140b
agneszhangmeng <mz435@cornell.edu>
2017-11-17 14:00:51 +0800

    Update redshift.sql

--- redshift.sql
+++ redshift.sql
@@ -31,29 +31,29 @@ order by wk
 
 SELECT 
 	A.skuseq,
-	A.day,
+	A.wk AS wk,
 	sum(A.dailysales) as sales,
 	sum(f.d1+f.d2+f.d3+f.d4+f.d5+f.d6+f.d7) Forecast
 FROM(
 	SELECT 
 		s.externalid as skuseq, 
-		to_char(o.orderedat, 'yyyymmdd') as day, 
 		d.iyyyy_1_wk AS wk, 
 		sum( oi.quantity ) as dailysales
 	FROM 
 		ods.ORDERS o join ods.ORDER_ITEMS oi on o.id = oi.orderid 
 		join ods.skus s on s.id = oi.skuid 
-		JOIN bimart.dim_date d ON trunc(d.dates) = to_date(f.rundt,'yyyymmdd' )
+		JOIN bimart.dim_date d ON trunc(d.dates) = trunc(o.orderedat) 
 	        where o.status <> 'CANCELED'  
-		and date(o.orderedat) >= to_date('20170701','yyyymmdd') 
-		and skuseq = 3057182
-	group by 1,2,3
+		and date(o.orderedat) >= to_date('20171001','yyyymmdd') 
+		and skuseq = 3120610
+	group by 1,2
 	order by wk
 ) A
-    left join sb_scm.scm_daily_forecast f 
-    on A.day=f.RUNDT and f.SKU = A.skuseq
-where 
-    F.RUNDT >= '20170701'
+left join sb_scm.scm_daily_forecast f on f.sku = A.skuseq 
+left join  bimart.dim_date d on trunc(d.dates) = to_date(f.rundt,'yyyymmdd' ) and A.wk = d.iyyyy_1_wk
+where f.sku = 3120610 and trunc(d.dates)  >= '2017-10-01'
 group by 1,2
-order by day 
+order by wk
+    
+
  
